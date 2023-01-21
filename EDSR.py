5fb8ecfab6ca45a81f631377d22cada6a4faf0a9
deepsky1215 <deepskyai1215@gmail.com>
2023-01-22 02:14:34 +0900

    Finish make a EDSR code but, need a training of model using main computer

--- EDSR.py
+++ EDSR.py
@@ -1,100 +1,202 @@
-# 잔차블럭에서 배치정규화를 제거해줌 = conv > relu > conv
-# 활성함수는 relu
-# psnr / L1 loss 사용
-
 import tensorflow as tf
-from keras.layers import Dense, Conv2D, Add,
+from tensorflow import keras
+from keras.layers import Add, Conv2D, Input, Lambda, Rescaling
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+img_size = 256
+channel = 3
+batch_size = 32
+epoch = 30
+AUTOTUNE = tf.data.AUTOTUNE
+
+div2k_data = tfds.image.Div2k(config="bicubic_x4")
+div2k_data.download_and_prepare()
+
+train = div2k_data.as_dataset(split="train", as_supervised=True)
+train_cache = train.cache()
+
+val = div2k_data.as_dataset(split="validation", as_supervised=True)
+val_cache = val.cache()
+
+def flip_left_right(lowres_img, highres_img):
+    rn = tf.random.uniform(shape=(), maxval=1)
+    return tf.cond(
+        rn < 0.5,
+        lambda: (lowres_img, highres_img),
+        lambda: (
+            tf.image.flip_left_right(lowres_img),
+            tf.image.flip_left_right(highres_img),
+        ),
+    )
 
 
-class EDSRModel(tf.keras.Model):
+def random_rotate(lowres_img, highres_img):
+    rn = tf.random.uniform(shape=(), maxval=4, dtype=tf.int32)
+    return tf.image.rot90(lowres_img, rn), tf.image.rot90(highres_img, rn)
+
+
+def random_crop(lowres_img, highres_img, hr_crop_size=96, scale=4):
+    lowres_crop_size = hr_crop_size // scale
+    lowres_img_shape = tf.shape(lowres_img)[:2]
+
+    lowres_width = tf.random.uniform(
+        shape=(), maxval=lowres_img_shape[1] - lowres_crop_size + 1, dtype=tf.int32
+    )
+    lowres_height = tf.random.uniform(
+        shape=(), maxval=lowres_img_shape[0] - lowres_crop_size + 1, dtype=tf.int32
+    )
+
+    highres_width = lowres_width * scale
+    highres_height = lowres_height * scale
+
+    lowres_img_cropped = lowres_img[
+        lowres_height : lowres_height + lowres_crop_size,
+        lowres_width : lowres_width + lowres_crop_size,
+    ]
+    highres_img_cropped = highres_img[
+        highres_height : highres_height + hr_crop_size,
+        highres_width : highres_width + hr_crop_size,
+    ]
+
+    return lowres_img_cropped, highres_img_cropped
+
+def dataset_object(dataset_cache, training=True):
+
+    ds = dataset_cache
+    ds = ds.map(
+        lambda lowres, highres: random_crop(lowres, highres, scale=4),
+        num_parallel_calls=AUTOTUNE,
+    )
+
+    if training:
+        ds = ds.map(random_rotate, num_parallel_calls=AUTOTUNE)
+        ds = ds.map(flip_left_right, num_parallel_calls=AUTOTUNE)
+    ds = ds.batch(batch_size)
+
+    if training:
+        ds = ds.repeat()
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
+
+train_ds = dataset_object(train_cache, training=True)
+val_ds = dataset_object(val_cache, training=False)
+
+
+lowres, highres = next(iter(train_ds))
+
+# High Resolution Images
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(highres[i].numpy().astype("uint8"))
+    plt.title(highres[i].shape)
+    plt.axis("off")
+
+# Low Resolution Images
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(lowres[i].numpy().astype("uint8"))
+    plt.title(lowres[i].shape)
+    plt.axis("off")
+
+
+class EDSR_model_builder(Model):
     def train_step(self, data):
-        # Unpack the data. Its structure depends on your model and
-        # on what you pass to `fit()`.
         x, y = data
 
-        with tf.GradientTape() as tape:
-            y_pred = self(x, training=True)  # Forward pass
-            # Compute the loss value
-            # (the loss function is configured in `compile()`)
-            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+        with tf.GradientTape as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred, self.losses)
 
-        # Compute gradients
-        trainable_vars = self.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        # Update metrics (includes the metric that tracks the loss)
+        trainable_var = self.trainable_variables
+        gradient = tape.gradient(loss, trainable_var)
+
+        self.optimizer.apply_gradients(zip(gradient, trainable_var))
         self.compiled_metrics.update_state(y, y_pred)
-        # Return a dict mapping metric names to current value
+
         return {m.name: m.result() for m in self.metrics}
 
-    def predict_step(self, x):
-        # Adding dummy dimension using tf.expand_dims and converting to float32 using tf.cast
-        x = tf.cast(tf.expand_dims(x, axis=0), tf.float32)
-        # Passing low resolution image to model
-        super_resolution_img = self(x, training=False)
-        # Clips the tensor from min(0) to max(255)
-        super_resolution_img = tf.clip_by_value(super_resolution_img, 0, 255)
-        # Rounds the values of a tensor to the nearest integer
-        super_resolution_img = tf.round(super_resolution_img)
-        # Removes dimensions of size 1 from the shape of a tensor and converting to uint8
-        super_resolution_img = tf.squeeze(
-            tf.cast(super_resolution_img, tf.uint8), axis=0
-        )
-        return super_resolution_img
+    def predict_step(self, data):
+        x = tf.cast(tf.expand_dims(data, axis=0), tf.float32)
+        pred = self(x, training=False)
+        pred = tf.clip_by_value(pred, 0, 255)
+        pred = tf.round(pred)
+        pred = tf.cast(tf.squeeze(pred, axis=0), tf.uint8)
 
+        return pred
 
-# Residual Block
-def ResBlock(inputs):
-    x = layers.Conv2D(64, 3, padding="same", activation="relu")(inputs)
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.Add()([inputs, x])
-    return x
+def res_block(Input_layer, filter=64):
+    res_x = Conv2D(filters=filter, kernel_size=3, activation='relu', padding='same')(Input_layer)
+    res_x = Conv2D(filters=filter, kernel_size=3, padding='same')(res_x)
+    # res_x = Lambda(function=lambda x: x * 0.1)(res_x)
+    result = Add()([Input_layer, res_x])
 
+    return result
 
-# Upsampling Block
-def Upsampling(inputs, factor=2, **kwargs):
-    x = layers.Conv2D(64 * (factor ** 2), 3, padding="same", **kwargs)(inputs)
-    x = tf.nn.depth_to_space(x, block_size=factor)
-    x = layers.Conv2D(64 * (factor ** 2), 3, padding="same", **kwargs)(x)
-    x = tf.nn.depth_to_space(x, block_size=factor)
-    return x
+def upsampling(Input_layer, filter=64, scale=2, **sky):
+    up_x = Conv2D(filter * (scale ** 2), kernel_size=3, padding='same', **sky)(Input_layer)
+    up_x = tf.nn.depth_to_space(up_x, block_size=scale)
+    up_x = Conv2D(filter * (scale ** 2), kernel_size=3, padding='same', **sky)(up_x)
+    up_x = tf.nn.depth_to_space(up_x, block_size=scale)
 
+    return up_x
 
-def make_model(num_filters, num_of_residual_blocks):
-    input_layer = layers.Input(shape=(None, None, 3))
-    x = layers.Rescaling(scale=1.0 / 255)(input_layer)
-    x = x_new = layers.Conv2D(num_filters, 3, padding="same")(x)
+def build_edsr(filter=64, num_res=16):
+    input = Input((None, None, channel))
+    x = Rescaling(scale=1.0/255.0)(input)
+    x = res_x = Conv2D(filter, kernel_size=3, padding='same')(x)
 
-    # 16 residual blocks
-    for _ in range(num_of_residual_blocks):
-        x_new = ResBlock(x_new)
+    for _ in range(num_res):
+        res_x = res_block(res_x)
 
-    x_new = layers.Conv2D(num_filters, 3, padding="same")(x_new)
-    x = layers.Add()([x, x_new])
+    res_x = Conv2D(filter, kernel_size=3, padding='same')(res_x)
+    x = Add()([x, res_x])
 
-    x = Upsampling(x)
-    x = layers.Conv2D(3, 3, padding="same")(x)
+    up_x = upsampling(x)
+    output = Conv2D(channel, kernel_size=3, padding='same')(up_x)
+    output = Rescaling(255)(output)
 
-    output_layer = layers.Rescaling(scale=255)(x)
-    return EDSRModel(input_layer, output_layer)
+    model = EDSR_model_builder(input, output)
 
+    return model
 
-model = make_model(num_filters=64, num_of_residual_blocks=16)
-Train the model
-# Using adam optimizer with initial learning rate as 1e-4, changing learning rate after 5000 steps to 5e-5
-optim_edsr = keras.optimizers.Adam(
-    learning_rate=keras.optimizers.schedules.PiecewiseConstantDecay(
-        boundaries=[5000], values=[1e-4, 5e-5]
-    )
+model = build_edsr()
+
+oprimizer = keras.optimizers.Adam(
+    learning_rate=keras.optimizers.schedules.PiecewiseConstantDecay(boundaries=[5000], values=[1e-4, 5e-5])
 )
-# Compiling model with loss as mean absolute error(L1 Loss) and metric as psnr
-model.compile(optimizer=optim_edsr, loss="mae", metrics=[PSNR])
-# Training for more epochs will improve results
-model.fit(train_ds, epochs=100, steps_per_epoch=200, validation_data=val_ds)
+
+def PSNR(y_true, y_pred):
+    return tf.image.psnr(y_true, y_pred, max_val=1.0)
+
+def SSIM(y_true, y_pred):
+    return tf.image.ssim(y_true, y_pred, max_val=1.0)
+
+model.compile(
+    loss = 'mae',
+    optimizer=oprimizer,
+    metrics=[PSNR, SSIM]
+)
+
+
+filename = 'checkpoint-epoch-{}-batch-{}-trial-001.h5'.format(epoch, batch_size)
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+earlystopping = EarlyStopping(monitor='val_loss', patience=5)
+
+
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epoch,
+    #steps_per_epoch=200,
+    callbacks=[checkpoint,earlystopping],
+    verbose=1
+)
 
 
 
-from keras.layers import Add, Conv2D, Input, Lambda
-from keras.models import Model
-from model.common import nomalize, pixel_shuffle, denormalize
