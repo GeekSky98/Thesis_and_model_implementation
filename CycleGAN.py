284cb081db627a6a72eda07e491a24e534801355
Deepsky <deepskyai1215@gmail.com>
2023-02-07 09:05:05 +0900

    This model takes a long time to learn.

--- CycleGAN.py
+++ CycleGAN.py
@@ -1,22 +1,20 @@
 import functools
-from glob import glob
 import tqdm
-import matplotlib.pyplot as plt
-import IPython.display as display
-import PIL
-import imageio
-import tensorflow.keras as keras
-from tensorflow.keras import layers
-import tensorflow_addons as tfa
-import sys
-# from experiment_utils import *
+from experiment_utils import *
 import tensorflow as tf
 import numpy as np
 from keras.layers import Conv2D, BatchNormalization, Activation, add, Input, Conv2DTranspose, ZeroPadding2D, LeakyReLU
 from tensorflow_addons.layers import InstanceNormalization
 from keras.models import Model
 from PIL import Image
-import os, random, keras
+import os, random
+from keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras import optimizers
+import IPython.display as display
+import imageio
+from glob import glob
+import IPython
+# %load_ext tensorboard
 
 input_shape = (128, 128, 3)
 hidden_layers = 3
@@ -32,12 +30,19 @@ gradient_penalty_mode = 'none'
 autotune = tf.data.AUTOTUNE
 
 cur_path = os.getcwd()
-data_path = os.path.join(cur_path, 'data\\gan\\facades\\facades')
+data_path = os.path.join(cur_path, 'data\\gan\\vangogh2photo\\vangogh2photo')
 
 train_a_path = os.path.join(data_path, 'trainA')
 train_b_path = os.path.join(data_path, 'trainB')
 test_a_path = os.path.join(data_path, 'testA')
 test_b_path = os.path.join(data_path, 'testB')
+
+cyclegan_path = os.path.join(cur_path, 'cyclegan')
+checkpoint_dir = os.path.join(cyclegan_path, 'checkpoint')
+output_dir = os.path.join(cyclegan_path, 'output')
+sample_dir = os.path.join(output_dir, 'sample_training')
+summary_train_dir = os.path.join(output_dir, 'summaries', 'train')
+train_summary_writer = tf.summary.create_file_writer(summary_train_dir)
 
 def get_image(path):
     dir = [file_name for file_name in os.listdir(path) if os.path.splitext(file_name)[-1] == '.jpg']
@@ -83,6 +88,7 @@ test_b = custom_dataloader(test_b, mode='test')
 
 train_ds = tf.data.Dataset.zip((train_a, train_b))
 test_ds = tf.data.Dataset.zip((test_a, test_b))
+len_dataset = max(len(train_a), len(train_b)) // n_batch
 
 def residual_block(input_layer):
 
@@ -156,3 +162,157 @@ Gen_b_to_a = generator()
 Disc_a = discriminator()
 Disc_b = discriminator()
 
+mse = MeanSquaredError()
+
+def get_gen_loss(fake):
+    f_loss = mse(tf.ones_like(fake), fake)
+    return f_loss
+
+def get_disc_loss(real, fake):
+    r_loss = mse(tf.ones_like(real), real)
+    f_loss = mse(tf.zeros_like(fake), fake)
+    return r_loss, f_loss
+
+cycle_loss = MeanAbsoluteError()
+identity_loss = MeanAbsoluteError()
+
+
+def gradient_penalty(tool, real, fake):
+    def interpolate(a, b):
+        shape = [tf.shape(a)[0]] + [1] * (a.shape.ndims - 1)
+        alpha = tf.random.uniform(shape=shape, minval=0., maxval=1.)
+        inter = a + alpha * (b - a)
+        inter.set_shape(a.shape)
+        return inter
+
+    x = interpolate(real, fake)
+    with tf.GradientTape() as tape:
+        tape.watch(x)
+        pred = tool(x)
+    gradient = tape.gradient(pred, x)
+    norm = tf.norm(tf.reshape(gradient, [tf.shape(gradient)[0], -1]), axis=1)
+    output = tf.reduce_mean((norm - 1.) ** 2)
+
+    return output
+
+gen_lr_scheduler = LinearDecay(LR, epochs * len_dataset, epoch_decay * len_dataset)
+disc_lr_scheduler = LinearDecay(LR, epochs * len_dataset, epoch_decay * len_dataset)
+
+gen_optimizer = optimizers.Adam(learning_rate=gen_lr_scheduler, beta_1=BETA_1)
+disc_optimizer = optimizers.Adam(learning_rate=disc_lr_scheduler, beta_1=BETA_1)
+
+def train_gen(A, B):
+    with tf.GradientTape() as tape:
+        A2B = Gen_a_to_b(A, training=True)
+        B2A = Gen_b_to_a(B, training=True)
+        A2B2A = Gen_b_to_a(A2B, training=True)
+        B2A2B = Gen_a_to_b(B2A, training=True)
+        A2A = Gen_b_to_a(A, training=True)
+        B2B = Gen_a_to_b(B, training=True)
+
+        disc_A2B_value = Disc_b(A2B, training=True)
+        disc_B2A_value = Disc_a(B2A, training=True)
+
+        gen_loss_A2B = get_gen_loss(disc_A2B_value)
+        gen_loss_B2A = get_gen_loss(disc_B2A_value)
+
+        cycle_loss_A2B2A = cycle_loss(A, A2B2A)
+        cycle_loss_B2A2B = cycle_loss(B, B2A2B)
+
+        identity_loss_A2A = identity_loss(A, A2A)
+        identity_loss_B2B = identity_loss(B, B2B)
+
+        generator_loss = (gen_loss_A2B+gen_loss_B2A) + (cycle_loss_A2B2A+cycle_loss_B2A2B) * cycle_loss_weight + \
+                   (identity_loss_A2A+identity_loss_B2B)*identity_loss_weight
+
+    generator_gradient = tape.gradient(generator_loss, Gen_a_to_b.trainable_variables + Gen_b_to_a.trainable_variables)
+    gen_optimizer.apply_gradients(zip(generator_gradient, Gen_a_to_b.trainable_variables +
+                                      Gen_b_to_a.trainable_variables))
+
+    return A2B, B2A, {'A2B_g_loss': gen_loss_A2B,
+                      'B2A_g_loss': gen_loss_B2A,
+                      'A2B2A_cycle_loss': cycle_loss_A2B2A,
+                      'B2A2B_cycle_loss': cycle_loss_B2A2B,
+                      'A2A_id_loss': identity_loss_A2A,
+                      'B2B_id_loss': identity_loss_B2B}
+
+def train_disc(A, B, A2B, B2A):
+    with tf.GradientTape() as tape:
+        A_value = Disc_a(A, training=True)
+        B2A_value = Disc_a(B2A, training=True)
+        B_value = Disc_b(B, training=True)
+        A2B_value = Disc_b(A2B, training=True)
+
+        disc_loss_A, disc_loss_B2A = get_disc_loss(A_value, B2A_value)
+        disc_loss_B, disc_loss_A2B = get_disc_loss(B_value, A2B_value)
+
+        discriminator_A_GP = gradient_penalty(functools.partial(Disc_a, training=True), A, B2A)
+        discriminator_B_GP = gradient_penalty(functools.partial(Disc_b, training=True), B, A2B)
+
+        discriminator_loss = (disc_loss_A+disc_loss_B2A) + (disc_loss_B+disc_loss_A2B) + \
+                             (discriminator_A_GP+discriminator_B_GP)*gradient_penalty_weight
+
+        discriminator_gradient = tape.gradient(discriminator_loss, Disc_a.trainable_variables+
+                                               Disc_b.trainable_variables)
+        disc_optimizer.apply_gradients(zip(discriminator_gradient, Disc_a.trainable_variables+
+                                           Disc_b.trainable_variables))
+
+        return {'A_d_loss': disc_loss_A + disc_loss_B2A,
+            'B_d_loss': disc_loss_B + disc_loss_A2B,
+            'D_A_gp': discriminator_A_GP,
+            'D_B_gp': discriminator_B_GP}
+
+A2B_pool = ItemPool(5)
+B2A_pool = ItemPool(5)
+
+def train_step(A, B):
+    A2B, B2A, gen_loss_total = train_gen(A, B)
+
+    A2B = A2B_pool(A2B)
+    B2A = B2A_pool(B2A)
+
+    disc_loss_total = train_disc(A, B, A2B, B2A)
+
+    return gen_loss_total, disc_loss_total
+
+def sample_image(A, B):
+    A2B = Gen_a_to_b(A, training=False)
+    B2A = Gen_b_to_a(B, training=False)
+    A2B2A = Gen_b_to_a(A2B, training=False)
+    B2A2B = Gen_a_to_b(B2A, training=False)
+    return A2B, B2A, A2B2A, B2A2B
+
+test_iter = iter(test_ds)
+
+checkpoint = Checkpoint(dict(G_A2B=Gen_a_to_b,
+                                G_B2A=Gen_b_to_a,
+                                D_A=Disc_a,
+                                D_B=Disc_b,
+                                G_optimizer=gen_optimizer,
+                                D_optimizer=disc_optimizer,
+                                ep_cnt=ep_cnt),
+                           checkpoint_dir,
+                           max_to_keep=5)
+
+with train_summary_writer.as_default():
+    for ep in tqdm.trange(epochs, desc='Epoch Loop'):
+        if ep < ep_cnt:
+            continue
+
+        ep_cnt.assign_add(1)
+
+        for A, B in tqdm.tqdm(train_ds, desc='Inner Epoch Loop', total=len_dataset):
+            G_loss_dict, D_loss_dict = train_step(A, B)
+
+            summary(G_loss_dict, step=gen_optimizer.iterations, name='generator_losses')
+            summary(D_loss_dict, step=gen_optimizer.iterations, name='discriminator_losses')
+            summary({'learning rate': gen_lr_scheduler.current_learning_rate}, step=gen_optimizer.iterations,
+                    name='learning rate')
+
+            if gen_optimizer.iterations.numpy() % 100 == 0:
+                A, B = next(test_iter)
+                A2B, B2A, A2B2A, B2A2B = sample_image(A, B)
+                img = immerge(np.concatenate([A, A2B, A2B2A, B, B2A, B2A2B], axis=0), n_rows=2)
+                imwrite(img, os.path.join(sample_dir, 'iter-%09d.jpg' % gen_optimizer.iterations.numpy()))
+
+        checkpoint.save(ep)
