190f07fbcb919ac5abcaaa003a7aa2ad732a3418
Deepsky <deepskyai1215@gmail.com>
2023-02-25 23:32:04 +0900

    ADD def in utils.py & i accidentally put the loss code in the evaluation. i changed it again.

--- YOLOv1/utils.py
+++ YOLOv1/utils.py
@@ -1,4 +1,56 @@
+import cv2
+import numpy as np
 import tensorflow as tf
+import colorsys
+from operator import itemgetter
+
+def draw_bounding_box_and_label_info(image, x_min, y_min, x_max, y_max, label, confidence, color):
+  draw_bounding_box(image, x_min, y_min, x_max, y_max, color)
+  draw_label_info(image, x_min, y_min, label, confidence, color)
+
+
+def draw_bounding_box(frame, x_min, y_min, x_max, y_max, color):
+  cv2.rectangle(
+    frame,
+    (x_min, y_min),
+    (x_max, y_max),
+    color, 3)
+
+
+def draw_label_info(frame, x_min, y_min, label, confidence, color):
+  text = label + ' ' + str('%.3f' % confidence)
+  bottomLeftCornerOfText = (x_min, y_min)
+  font = cv2.FONT_HERSHEY_SIMPLEX
+  fontScale = 0.8
+  fontColor = color
+  lineType = 2
+
+  cv2.putText(frame, text,
+              bottomLeftCornerOfText,
+              font,
+              fontScale,
+              fontColor,
+              lineType)
+
+def find_max_confidence_bounding_box(bounding_box_info_list):
+  bounding_box_info_list_sorted = sorted(bounding_box_info_list,
+                                                   key=itemgetter('confidence'),
+                                                   reverse=True)
+  max_confidence_bounding_box = bounding_box_info_list_sorted[0]
+
+  return max_confidence_bounding_box
+
+
+def yolo_format_to_bounding_box_dict(xcenter, ycenter, box_w, box_h, class_name, confidence):
+  bounding_box_info = {}
+  bounding_box_info['left'] = int(xcenter - (box_w / 2))
+  bounding_box_info['top'] = int(ycenter - (box_h / 2))
+  bounding_box_info['right'] = int(xcenter + (box_w / 2))
+  bounding_box_info['bottom'] = int(ycenter + (box_h / 2))
+  bounding_box_info['class_name'] = class_name
+  bounding_box_info['confidence'] = confidence
+
+  return bounding_box_info
 
 
 def iou(yolo_pred_boxes, ground_truth_boxes):
@@ -27,3 +79,17 @@ def iou(yolo_pred_boxes, ground_truth_boxes):
   square2 = (boxes2[2] - boxes2[0]) * (boxes2[3] - boxes2[1])
 
   return inter_square / (square1 + square2 - inter_square + 1e-6)
+
+
+def generate_color(num_classes):
+  hsv_tuples = [(x / num_classes, 1., 1.)
+                for x in range(num_classes)]
+  colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+  colors = list(
+    map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+        colors))
+  np.random.seed(10101)
+  np.random.shuffle(colors)
+  np.random.seed(None)
+
+  return
